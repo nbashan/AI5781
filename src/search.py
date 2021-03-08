0e6b61912e4042285896a3723500f736d9634910
נתנאל בשן <nbashan@g.jct.ac.il>
2021-03-08 09:12:37 +0200

    elyasaf

--- src/search.py
+++ src/search.py
@@ -1,5 +1,27 @@
 # Netanel Bashan, 323056077, netanebashan12@gmail.com
 # Elyasaf Dimant, 204006415, elyasafdi@gmail.com
+
+
+#2.)        h1 hurisitcs results
+#       omekMax3 == 230
+#       average mispar bdikot 3 == 33.56
+#
+#       omekMax4 == 115,304
+#       average mispar bdikot 4 == 10,148.25
+#
+#
+#4.)        h2 hurisitcs results
+#       omekMax3 == 76
+#       average mispar bdikot 3 == 23.5
+#
+#       omekMax4 == 113,520
+#       average mispar bdikot 4 == 4094.64
+#
+#          CONCLUSIONS:
+#   A* using h2 manhaton is more optimal than using h1
+#   WE CAN SEE IT BY THAT EVERY CATEGORY IN H2 IS SMALLER THAN H1
+
+
 #search
 import state
 import frontier
@@ -48,3 +70,5 @@ print("averageMisBdik3\n")
 print(misBdik3/100)
 print("averageMisBdik4\n")
 print(misBdik4/100)
+
+
