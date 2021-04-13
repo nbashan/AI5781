e8ee55c98e0b5fc9bc579a69e9189c926e204bf8
אליסף דימנט <edimant@g.jct.ac.il>
2021-04-13 17:30:00 +0300

    need comments!!!!!!!!!

--- src/assignment 2/Main.py
+++ src/assignment 2/Main.py
@@ -8,7 +8,7 @@ BritishCounter_4 = 0
 BritishCounter_6 = 0
 ForwardCounter_maxN = 27
 HeuristicAverage_27 = 0
-BritishCounter_15 = 0
+# BritishCounter_15 = 0
 ForwardIterations_15 = 0
 ForwardMoves_15 = 0
 HeuristicAverage_15 = 0
@@ -18,40 +18,46 @@ for i in range(20):
     BritishCounter_4 += BritishMuseum.start(4)
     BritishCounter_6 += BritishMuseum.start(6)
     HeuristicAverage_27 += HeuristicRepair.start(ForwardCounter_maxN)
-    #BritishCounter_15 += BritishMuseum.start(15)
+    # BritishCounter_15 += BritishMuseum.start(15)
     ForwardIterations_15temp, ForwardMoves_15temp = ForwardChecking.start(15)
     ForwardIterations_15 += ForwardIterations_15temp
     ForwardMoves_15 += ForwardMoves_15temp
     HeuristicAverage_15 += HeuristicRepair.start(15)
 
 ForwardIterator, ForwardMoves = ForwardChecking.start(ForwardCounter_maxN)
+ForwardIterator_8, ForwardMoves_8 = ForwardChecking.start(8)
 
+print()
 print("Question 2:")
-print("BritishMuseum:")
+print("British Museum:")
 print("Average iterations in British (4) = ", BritishCounter_4 / 20)
 print("Average iterations in British (6) = ", BritishCounter_6 / 20)
 
+print()
 print("Question 3:")
 print("Forward Checking:")
 print("As remember the counters of DFS were:")
 print("# of iterations: 982")
 print("# of queens placed + backtracks: 218")
-print("How much did you save compared to the simple DFS algorithm ?")
-print("Answer: None!")
-print("         number_of_iterations: ", ForwardIterator)
-print("         number_of_moves: ", ForwardMoves)
-print("To what extent did you manage to get a solution ?")
-print("Answer: n = ", ForwardCounter_maxN)
+print("Q: How much did you save compared to the simple DFS algorithm ?")
+print("A: None!")
+print("         # of iterations: ", ForwardIterator_8)
+print("         # of queens placed + backtracks: ", ForwardMoves_8)
+print("Q: To what extent did you manage to get a solution ?")
+print("A: n = ", ForwardCounter_maxN)
 
+print()
 print("Question 4:")
 print("Heuristic Repair:")
-print("Were you able to improve the average performance of 20 runs across the algorithm with Forward Checking ?")
-print("Answer: Yes!, now the average is: ", HeuristicAverage_27 / 20)
+print("Q: Were you able to improve the average performance of 20 runs across the algorithm with Forward Checking ?")
+print("A: Yes!, # of iterations using Forward Checking was: ", ForwardIterator, " (when size = 27)")
+print("now, using the heuristic, the average is: ", HeuristicAverage_27 / 20)
 
+print()
 print("Question 5:")
-print("What were the values of your masses for n = 15 on average?")
-print("Answer:")
-print("British: ", BritishCounter_15 / 20)
-print("Forward: ", "number_iterations: ", ForwardIterations_15 / 20)
-print("         number_moves: ", ForwardMoves_15 / 20)
+print("Q: What were the values of your counters for n = 15 on average?")
+print("A:")
+# print("British: ", BritishCounter_15 / 20)
+print("Forward: ", "# of iterations: ", ForwardIterations_15 / 20)
+print("          # of moves: ", ForwardMoves_15 / 20)
 print("Heuristic: ", HeuristicAverage_15 / 20)
